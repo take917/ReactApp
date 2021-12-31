8e2f745918e4e88460a2f66b4938e0de04cb1946
take917 <35625934+take917@users.noreply.github.com>
2021-12-31 16:07:53 +0900

    ref モーダルに文字が出るように調整

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,7 +3,7 @@ import { Button } from "./components/button";
 import styled from "styled-components";
 import { TabBodyContainer } from "./components/tab-body-container";
 import { FormModal } from "./FormModal";
-
+import { Hint } from "./Hint";
 const Label = styled.label`
   display: flex;
   color: #757575;
@@ -41,6 +41,7 @@ export const Form = ({ onAddLang }) => {
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
+          <Hint />
         </div>
         <ButtonContainer>
           <FormButton>追加</FormButton>
